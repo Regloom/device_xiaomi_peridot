3a32b2acb4febc2a7b3ce148bc8e04b0c966cb4a
Guidix <guidixm@gmail.com>
2025-11-17 14:42:29 +0200

    peridot: Update from OS2.0.206
    Signed-off-by: Guidix <guidixm@gmail.com>

--- lineage_peridot.mk
+++ lineage_peridot.mk
@@ -30,8 +30,8 @@ PRODUCT_SYSTEM_DEVICE := peridot
 
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="peridot_global-user 15 AQ3A.240912.001 OS2.0.205.0.VNPMIXM release-keys" \
-    BuildFingerprint=POCO/peridot_global/peridot:15/AQ3A.240912.001/OS2.0.205.0.VNPMIXM:user/release-keys \
+    BuildDesc="peridot_global-user 15 AQ3A.240912.001 OS2.0.206.0.VNPMIXM release-keys" \
+    BuildFingerprint=POCO/peridot_global/peridot:15/AQ3A.240912.001/OS2.0.206.0.VNPMIXM:user/release-keys \
     DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
 
