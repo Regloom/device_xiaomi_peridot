def0eb51d4ad2baa9f83d7abe7d5f17e15471911
Guidix <guidixm@gmail.com>
2025-12-31 16:22:10 +0200

    peridot: Update vendor blobs from OS2.0.208.0.VNPMIXM
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- lineage_peridot.mk
+++ lineage_peridot.mk
@@ -30,8 +30,8 @@ PRODUCT_SYSTEM_DEVICE := peridot
 
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="peridot_global-user 15 AQ3A.240912.001 OS2.0.207.0.VNPMIXM release-keys" \
-    BuildFingerprint=POCO/peridot_global/peridot:15/AQ3A.240912.001/OS2.0.207.0.VNPMIXM:user/release-keys \
+    BuildDesc="peridot_global-user 15 AQ3A.240912.001 OS2.0.208.0.VNPMIXM release-keys" \
+    BuildFingerprint=POCO/peridot_global/peridot:15/AQ3A.240912.001/OS2.0.208.0.VNPMIXM:user/release-keys \
     DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
 
