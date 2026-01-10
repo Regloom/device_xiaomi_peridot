7e48f4e4eca1a7446236f9d5b939101ddfecbc37
Sebastiano Barezzi <seba@sebaubuntu.dev>
2026-01-10 07:54:33 +0100

    peridot: Use hardware/xiaomi FCM
    Change-Id: Ib1dcdfe20ef5ffaf1b3b407b47790ee029a2755f
    Signed-off-by: Guidix <guidixm@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,8 +82,8 @@ TARGET_USES_VULKAN := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(DEVICE_PATH)/configs/hidl/compatibility_matrix.device.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/framework_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
