e7f2d1b199c240b80eea2592ae22e1d250fddb80
regloom <egor.tokarev@outlook.com>
2026-01-24 21:06:36 +0400

    peridot: parts included twice

--- device.mk
+++ device.mk
@@ -22,9 +22,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # pKVM
 $(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk)
 
-# Parts
-$(call inherit-product, packages/apps/XiaomiParts/parts.mk)
-
 # Qualcomm
 $(call soong_config_set,rfs,mpss_firmware_symlink_target,modem_firmware)
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
