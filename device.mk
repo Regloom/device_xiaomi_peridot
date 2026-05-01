813abaa8d6fde4db249f500bca9dc8753d221f89
zenin1504 <kayozzprojects@yandex.com>
2026-05-01 20:28:32 +0800

    peridot: Build Charging Service
    Change-Id: Iadb736a5541b49988c23febe1983d06b
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>
    Signed-off-by: Guidix <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -160,6 +160,10 @@ PRODUCT_COPY_FILES += \
 # Characteristics
 PRODUCT_CHARACTERISTICS := nosdcard
 
+# Charging
+PRODUCT_PACKAGES += \
+    charging
+
 # DeviceAsWebcam
 PRODUCT_PACKAGES += \
     DeviceAsWebcam
