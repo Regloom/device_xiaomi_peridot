444f942818e1b33418a51edc67970607ae5e92cc
Guidix <guidixm@gmail.com>
2026-05-08 15:12:25 +0300

    Revert "peridot: Build Thermal Profiles App"
    This reverts commit 821ec0207f8e3f5e7c33cb603748bd35738dfee2.

--- device.mk
+++ device.mk
@@ -399,7 +399,6 @@ include packages/apps/XiaomiParts/parts.mk
 TARGET_USE_CLEARSPEAKER := true
 TARGET_USE_SATURATIONSLIDER := true
 TARGET_USE_KPROFILES := true
-TARGET_USE_THERMALPROFILES := true
 TARGET_SUPPORTS_HTSR := true
 
 # Partitions
